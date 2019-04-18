6032db003cd8f6fdfbf4645e4cd2a166d2968bce
gonbe <rr7wswjc.mt7dcikm@icloud.com>
2019-04-19 06:54:25 +0900

    コメント追加

---
+++ 配列/オブジェクト/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+{
+  // オブジェクト
+
+  // const player = ['taguchi', 32];
+// オブジェクトにすることで内容をわかりやすく
+// 名前（キー）と値で設定できる
+  const player = {
+    name: 'taguchi',
+    score: 32,
+  };
+
+  console.log(player);
+
+}
---
+++ 配列/配列要素/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+{
+  // 配列
+
+  const scores = [80, 90, 40];
+番号で指定する
+  // console.log(scores[1]); // 90
+// 指定して内容を変える
+  // scores[2] = 100;
+  // console.log(scores);
+// 配列の中身は変更できても再代入はできない
+  // scores = 1;
+// 配列の数
+  console.log(scores.length); // 3
+}
